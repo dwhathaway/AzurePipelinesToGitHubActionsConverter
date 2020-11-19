3fa239393a9f4e02f02d4e9fc774cffde9a8a58c
Nate Rickard <NateRickard@github.com>
2020-11-19 17:37:27 -0500

    formatting

--- src/AzurePipelinesToGitHubActionsConverter.Core/Conversion/Serialization/GenericObjectSerialization.cs
+++ src/AzurePipelinesToGitHubActionsConverter.Core/Conversion/Serialization/GenericObjectSerialization.cs
@@ -4,20 +4,21 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.Conversion.Serialization
 {
     public static class GenericObjectSerialization
     {
-        //Read in a YAML file and convert it to a T object
+        // Read in a YAML file and convert it to a T object
         public static T DeserializeYaml<T>(string yaml)
         {
-            IDeserializer deserializer = new DeserializerBuilder().Build();
+            var deserializer = new DeserializerBuilder().Build();
             T yamlObject = deserializer.Deserialize<T>(yaml);
+
             return yamlObject;
         }
 
-        //Write a YAML file using the T object
+        // Write a YAML file using the T object
         public static string SerializeYaml<T>(T obj)
         {
-            //Convert the object into a YAML document
-            ISerializer serializer = new SerializerBuilder()
-                .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults) //New as of YamlDotNet 8.0.0: https://github.com/aaubry/YamlDotNet/wiki/Serialization.Serializer#configuredefaultvalueshandlingdefaultvalueshandling. This will not show null properties, e.g. "app-name: " will not display when the value is null, as the value is nullable
+            // Convert the object into a YAML document
+            var serializer = new SerializerBuilder()
+                .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults) // New as of YamlDotNet 8.0.0: https://github.com/aaubry/YamlDotNet/wiki/Serialization.Serializer#configuredefaultvalueshandlingdefaultvalueshandling. This will not show null properties, e.g. "app-name: " will not display when the value is null, as the value is nullable
                 .Build();
             string yaml = serializer.Serialize(obj);
 
