16109c2d626ce922748c3285630b37296bdeb26a
Nate Rickard <NateRickard@github.com>
2021-01-22 17:43:32 -0500

    updated stage->job dependency propagation

--- src/AzurePipelinesToGitHubActionsConverter.Core/Conversion/StagesProcessing.cs
+++ src/AzurePipelinesToGitHubActionsConverter.Core/Conversion/StagesProcessing.cs
@@ -73,47 +73,57 @@ namespace AzurePipelinesToGitHubActionsConverter.Core.Conversion
                     // Giant nested loop ahead. Loop through stages, looking for all jobs
                     int jobIndex = 0;
 
-                    foreach (Stage stage in stages)
+                    for (int stageIndex = 0; stageIndex < stages.Count; stageIndex++)
                     {
+                        var stage = stages[stageIndex];
+
                         if (stage.jobs != null)
                         {
                             for (int i = 0; i < stage.jobs.Length; i++)
                             {
-                                jobs[jobIndex] = stage.jobs[i];
+                                var job = jobs[jobIndex] = stage.jobs[i];
 
-                                // propagate stage dependency to the Job level
+                                // propagate stage dependency to the Job level...
+                                // First - do we have a specific job dependency that's been declared?
                                 if (stage.dependsOn?.Length > 0)
                                 {
                                     var dependsOnJobs = stages.Where(s => stage.dependsOn.Contains(s.stage)).Select(s => s.jobs.Last());
-                                    jobs[jobIndex].dependsOn = dependsOnJobs.Select(j => j.job).ToArray();
+                                    job.dependsOn = dependsOnJobs.Select(j => j.job).ToArray();
+                                }
+                                else if (stageIndex > 0) // otherwise, we still need to set up a dependency on the previous stage/job, to match ADO's default stage behavior
+                                {
+                                    if (job.dependsOn == null || job.dependsOn.Length == 0) // do nothing if Job-level dependency is already configured
+                                    {
+                                        job.dependsOn = new string[] { stages[stageIndex - 1].jobs.Last().job };
+                                    }
                                 }
 
                                 if (stage.variables != null)
                                 {
-                                    if (jobs[jobIndex].variables == null)
+                                    if (job.variables == null)
                                     {
-                                        jobs[jobIndex].variables = new OrderedDictionary();
+                                        job.variables = new OrderedDictionary();
                                     }
 
                                     foreach (DictionaryEntry stageVariable in stage.variables)
                                     {
                                         // Add the stage variable if it doesn't already exist
-                                        if (!jobs[jobIndex].variables.Contains(stageVariable.Key))
+                                        if (!job.variables.Contains(stageVariable.Key))
                                         {
-                                            jobs[jobIndex].variables.Add(stageVariable.Key, stageVariable.Value);
+                                            job.variables.Add(stageVariable.Key, stageVariable.Value);
                                         }
                                     }
                                 }
 
                                 if (stage.condition != null)
                                 {
-                                    jobs[jobIndex].condition = stage.condition;
+                                    job.condition = stage.condition;
                                 }
 
                                 // Get the job name
                                 string jobName = ConversionUtility.GenerateJobName(stage.jobs[i], jobIndex);
                                 // Rename the job, using the stage name as prefix, so that we keep the job names unique
-                                jobs[jobIndex].job = stage.stage + "_Stage_" + jobName;
+                                job.job = stage.stage + "_Stage_" + jobName;
                                 jobIndex++;
                             }
                         }
